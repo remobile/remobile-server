e1142fb657e5c4d4bfd7fad40f74ba53d3e860d6
fov42550564 <42550564@qq.com>
2016-08-28 22:26:34 +0800

    添加领取详情

--- lib/shared/screens/admin/screens/task/allTasks/components/index.jsx
+++ lib/shared/screens/admin/screens/task/allTasks/components/index.jsx
@@ -1,10 +1,10 @@
 import React, {PropTypes} from 'react';
-import {  Form, Table, Modal } from 'antd';
+import {  Table, Modal, Collapse } from 'antd';
 import _ from 'lodash';
 import moment from 'moment';
-const FormItem = Form.Item;
+const Panel = Collapse.Panel;
 
-const STATETEXT = [{text: '未领取', color: '#CDC9C9'}, {text: '进行中', color: '#EE3B3B'}, {text: '已完成', color: '#B3EE3A'}];
+const STATETEXT = [null, {text: '待提交', color: '#CDC9C9'}, {text: '待审核', color: '#EE3B3B'}, {text: '已完成', color: '#B3EE3A'}];
 
 export default class AllTasks extends React.Component {
     static fragments = {
@@ -14,24 +14,23 @@ export default class AllTasks extends React.Component {
             address: 1,
             description: 1,
             reward: 1,
-            state: 1,
             startTime: 1,
             endTime: 1,
-            acceptTime: 1,
-            submitTime: 1,
-            passTime: 1,
+            acceptList: 1,
         }
     };
     static columns = [{
         title: '名称',
         dataIndex: 'name',
     }, {
-        title: '状态',
-        dataIndex: 'state',
-        render: (i) => <span style={{color:STATETEXT[i].color}}>{STATETEXT[i].text}</span>,
-    }, {
         title: <span>赏金(<span style={{color:'red'}}>元</span>)</span>,
         dataIndex: 'reward',
+    }, {
+        title: '状态',
+        dataIndex: '',
+        render: (text, record, index) => {
+            return record.acceptList.length ? <span style={{color:'#66CD00'}}>有领取</span>: <span style={{color:'red'}}>无人问津</span>;
+        },
     }, {
         title: '开始时间',
         dataIndex: 'startTime',
@@ -41,25 +40,46 @@ export default class AllTasks extends React.Component {
         dataIndex: 'endTime',
         render: (text) => moment(new Date(text)).format('YYYY-MM-DD HH:mm:ss'),
     }];
+    static childColumns = [{
+        title: '领取人',
+        dataIndex: 'acceptor',
+        render: (text) => {
+            return text.phone;
+        },
+    }, {
+        title: '状态',
+        dataIndex: 'state',
+        render: (i) => <span style={{color:STATETEXT[i].color}}>{STATETEXT[i].text}</span>,
+    }];
     expandedRowRender(record) {
-        const {address, description} = record;
-        const formItemLayout = {labelCol: { span: 2}, wrapperCol: { span: 12}};
-        return (
-            <Form horizontal>
-                <FormItem
-                    {...formItemLayout}
-                    label="拍摄地址:"
-                    >
-                    <p className="ant-form-text">{address}</p>
-                </FormItem>
-                <FormItem
-                    {...formItemLayout}
-                    label="拍摄要求:"
-                    >
-                    <p className="ant-form-text">{description}</p>
-                </FormItem>
-            </Form>
-        )
+        const {address, description, acceptList} = record;
+        if (acceptList.length) {
+            const list = _.map(acceptList, (item, i)=>{return {...eval('('+item+')'), key: i}});
+            return (
+                <Collapse accordion>
+                    <Panel header={'拍摄地址:'} key="1">
+                        <p>{address}</p>
+                    </Panel>
+                    <Panel header={'拍摄要求:'} key="2">
+                        <p>{description}</p>
+                    </Panel>
+                    <Panel header={'领取详情:'} key="3">
+                        <Table columns={AllTasks.childColumns} dataSource={list} pagination={false} size="small" />
+                    </Panel>
+                </Collapse>
+            )
+        } else {
+            return (
+                <Collapse accordion>
+                    <Panel header={'拍摄地址:'} key="1">
+                        <p>{address}</p>
+                    </Panel>
+                    <Panel header={'拍摄要求:'} key="2">
+                        <p>{description}</p>
+                    </Panel>
+                </Collapse>
+            )
+        }
     }
     render () {
         const tasks = _.map(this.props.tasks, (task, i)=>({...task, key: i}));
