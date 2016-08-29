3ae4442a371f7b9417fd526387c9da951c03b51d
fov42550564 <42550564@qq.com>
2016-08-29 19:53:50 +0800

    添加appManage

---
+++ lib/shared/screens/admin/screens/appManage/appList/components/index.jsx
@@ -0,0 +1,11 @@
+import React, {PropTypes} from 'react';
+import styles from './index.less';
+
+export default class AppManage extends React.Component {
+    render () {
+        return (
+            <div className={styles.container}>
+            </div>
+        )
+    }
+}
