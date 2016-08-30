003563680f7b7899a3ef9eea9d170d1e5a273b06
fov42550564 <42550564@qq.com>
2016-08-31 00:03:38 +0800

    添加modal测试

--- lib/shared/screens/admin/screens/home/components/index.jsx
+++ lib/shared/screens/admin/screens/home/components/index.jsx
@@ -1,10 +1,31 @@
 import React, {PropTypes} from 'react';
+import { Modal, Button, Spin } from 'antd';
 import styles from './index.less';
 
 export default class Home extends React.Component {
+    state = {
+        passwordModalVisile: false,
+    }
+    handleSubmit() {
+        this.setState({passwordModalVisile: true});
+    }
     render () {
         return (
             <div className={styles.container}>
+                <Button type="primary" onClick={::this.handleSubmit}>登录</Button>
+                <Modal
+                    visible={this.state.passwordModalVisile}
+                    footer={null}
+                    className={styles.container}
+                    wrapClassName="vertical-center-modal"
+                    closable={false}
+                    >
+                    <div style={{textAlign:'center', padding: 250}}>
+                        <div style={{textAlign:'center', backgroundColor:'white', width:50, height:50, borderRadius:25, padding:14}}>
+                            <Spin />
+                        </div>
+                    </div>
+                </Modal>
             </div>
         )
     }
