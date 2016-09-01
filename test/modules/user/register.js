2ea15788610dd2f02fbb2bc386d3f649a40880a0
fov42550564 <42550564@qq.com>
2016-09-01 10:40:52 +0800

    完成对用户注册的整改

--- test/modules/user/register.js
+++ test/modules/user/register.js
@@ -3,6 +3,7 @@ var post = require('../../utils/post');
 var param = {
     phone: '18085192480',
     password: '123',
+    email: '42550564@qq.com',
 };
 
 post('register', param);
--- test/modules/user/userList.js
+++
@@ -1,3 +0,0 @@
-var post = require('../../utils/post');
-
-post('userList');
